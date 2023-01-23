880220a51faa493657968877e1af8ead0e758a8b
renanjorge <renan.haz@outlook.com>
2023-01-23 08:40:58 -0300

    Refatora o productView no javascript

--- Store.Application/wwwroot/js/site.js
+++ Store.Application/wwwroot/js/site.js
@@ -1,6 +1,9 @@
 ﻿"use strict";
 
-$(document).ready(function () {
+const productViewManager = function () {
+    let $table;
+
+    const $list = $("#lst-products");
     const $modal = $("#rgt-prd-modal");
     const $form = $("#frm-products");
 
@@ -11,48 +14,7 @@ $(document).ready(function () {
     const $category = $("#frm-products #category");
     const $isPerishable = $("#frm-products #is-perishable");
 
-    let $table = $('#lst-products').DataTable({
-        serverSide: true,
-        processing: true,
-        searching: false,
-        ordering: false,
-        ajax: {
-            url: 'products/list',
-            type: 'POST'
-        },
-        columns: [
-            { "data": "id" },
-            { "data": "name" },
-            { "data": "description" },
-            { "data": "categoryProductName" },
-            { "data": "active" },
-            { "data": "perishable" },
-            {
-                "data": null,
-                "render": function (data, type, row) {
-                    let btnUpdate = "<button type='button' class='btn-upt-prd btn btn-success btn-sm' data-id=" + data.id + ">Atualizar</button>";
-                    let btnDelete = "<button type='button' class='btn-del-prd btn btn-danger btn-sm' data-id=" + data.id + ">Deletar</button>";
-
-                    return btnUpdate + " " + btnDelete;
-                }
-            }
-        ],
-        language: {
-            url: '../lib/datatables/plug-ins/language/pt-BR.json'
-        }
-    });
-
-    $("#btn-sv-product").on("click", function () {
-
-        let data = {
-            Id: $id.val() === undefined || $id.val() === "" ? 0 : $id.val(),
-            IsActive: $isActive.val() === "true" || $isActive.val() === "",
-            Name: $name.val(),
-            Description: $description.val(),
-            CategoryProductId: $category.val(),
-            IsPerishable: $isPerishable.prop("checked")
-        };
-
+    const saveProduct = function (data) {
         if ($form.validate().form()) {
             $.ajax({
                 url: "/products",
@@ -67,12 +29,11 @@ $(document).ready(function () {
                 }
             });
         }
-        
-    });
+    };
 
-    $("#lst-products").on("click", ".btn-upt-prd", function () {
+    const getEditProduct = function (id) {
         $.ajax({
-            url: "/products/" + $(this).data("id"),
+            url: "/products/" + id,
             type: "GET",
             dataType: "json",
             success: function (result) {
@@ -87,20 +48,85 @@ $(document).ready(function () {
                 $modal.modal("show");
             }
         });
-    });
+    };
 
     // CRIAR CONFIRMAÇÃO DE EXCLUSÃO
-    $("#lst-products").on("click", ".btn-del-prd", function () {
+    const deleteProduct = function (id) {
         $.ajax({
             type: "DELETE",
-            url: "products/" + $(this).data("id"),
+            url: "products/" + id,
             success: function () {
                 $table.ajax.reload();
             }
         });
+    };
+
+    const resetForm = function () {
+        $form[0].reset();
+    };
+
+    const datatableInit = function () {
+        $table = $('#lst-products').DataTable({
+            serverSide: true,
+            processing: true,
+            searching: false,
+            ordering: false,
+            ajax: {
+                url: 'products/list',
+                type: 'POST'
+            },
+            columns: [
+                { "data": "id" },
+                { "data": "name" },
+                { "data": "description" },
+                { "data": "categoryProductName" },
+                { "data": "active" },
+                { "data": "perishable" },
+                {
+                    "data": null,
+                    "render": function (data, type, row) {
+                        let btnUpdate = "<button type='button' class='btn-upt-prd btn btn-success btn-sm' data-id=" + data.id + ">Atualizar</button>";
+                        let btnDelete = "<button type='button' class='btn-del-prd btn btn-danger btn-sm' data-id=" + data.id + ">Deletar</button>";
+
+                        return btnUpdate + " " + btnDelete;
+                    }
+                }
+            ],
+            language: {
+                url: '../lib/datatables/plug-ins/language/pt-BR.json'
+            }
+        });
+    };
+
+    $(document).ready(function () {
+        datatableInit();
+
+        $("#btn-sv-product").on("click", function () {
+            let data = {
+                Id: $id.val() === undefined || $id.val() === "" ? 0 : $id.val(),
+                IsActive: $isActive.val() === "true" || $isActive.val() === "",
+                Name: $name.val(),
+                Description: $description.val(),
+                CategoryProductId: $category.val(),
+                IsPerishable: $isPerishable.prop("checked")
+            };
+
+            saveProduct(data);
+        });
+
+        $list.on("click", ".btn-upt-prd", function () {
+            let id = $(this).data("id");
+            getEditProduct(id);
+        });
+
+        $list.on("click", ".btn-del-prd", function () {
+            let id = $(this).data("id");
+            deleteProduct(id);
+        });
+
+        $("#rgt-prd-modal .close").on("click", function () {
+            resetForm();
+        });
     });
 
-    $("#rgt-prd-modal .close").on("click", function () {
-        $form[0].reset();
-    });
-});
+}();
